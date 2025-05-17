5202d000d1189d6a485437ebfdfebf407f9a62d0
Keshav-1973 <keshavkhetan730@gmail.com>
2025-05-17 19:13:02 +0530

    hello13

--- src/middlewares/verifyShopifyHmac.ts
+++ src/middlewares/verifyShopifyHmac.ts
@@ -7,21 +7,21 @@ dotenv.config();
 const { SHOPIFY_SECRET_KEY = "" } = process.env;
 
 const verifyShopifyHmac = (req: Request, res: Response, next: NextFunction) => {
-  const hmac = req.get("X-Shopify-Hmac-Sha256");
+  const hmacHeader = req.get("X-Shopify-Hmac-Sha256");
+  const body = req.body;
 
-  if (!hmac || !SHOPIFY_SECRET_KEY) {
-    console.log("❌ Missing HMAC or SECRET_KEY");
-    return res.sendStatus(403);
-  }
-
-  const generatedHash = crypto
-    .createHmac("SHA256", SHOPIFY_SECRET_KEY)
-    .update(JSON.stringify(req.body), "utf8")
+  const hash = crypto
+    .createHmac("sha256", SHOPIFY_SECRET_KEY)
+    .update(body, "utf8")
     .digest("base64");
 
-  if (generatedHash !== hmac) {
-    console.log("❌ HMAC verification failed!");
-    return res.sendStatus(403);
+  console.log(hmacHeader, "....", hash, "....", SHOPIFY_SECRET_KEY);
+
+  if (hash === hmacHeader) {
+    console.log("✅ Verified Shopify Webhook");
+  } else {
+    console.warn("❌ Could not verify Shopify HMAC");
+    res.status(401).send("Unauthorized");
   }
 
   next();
