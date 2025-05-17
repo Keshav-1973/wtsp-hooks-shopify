64cc0420eacbfc93d652808a03b8b7fd5093f1e0
Keshav-1973 <keshavkhetan730@gmail.com>
2025-05-17 18:44:56 +0530

    hello13

--- src/middlewares/verifyShopifyHmac.ts
+++ src/middlewares/verifyShopifyHmac.ts
@@ -8,17 +8,18 @@ const { SHOPIFY_SECRET_KEY = "" } = process.env;
 
 const verifyShopifyHmac = (req: Request, res: Response, next: NextFunction) => {
   const hmac = req.get("X-Shopify-Hmac-Sha256");
+
   if (!hmac || !SHOPIFY_SECRET_KEY) {
     console.log("❌ Missing HMAC or SECRET_KEY");
     return res.sendStatus(403);
   }
 
-  const computedHmac = crypto
-    .createHmac("sha256", SHOPIFY_SECRET_KEY)
-    .update(req.rawBody.toString("utf8"))
+  const generatedHash = crypto
+    .createHmac("SHA256", SHOPIFY_SECRET_KEY)
+    .update(JSON.stringify(req.body), "utf8")
     .digest("base64");
 
-  if (computedHmac !== hmac) {
+  if (generatedHash !== hmac) {
     console.log("❌ HMAC verification failed!");
     return res.sendStatus(403);
   }
